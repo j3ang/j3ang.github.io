fbbf368a84e821ec8f93dfd6755063a3fb8a41be
Alex Wang <j3ang@gmail.com>
2021-01-09 13:21:01 -0600

    deploy

---
+++ books/payload.js
@@ -0,0 +1,1 @@
+window.__NUXT__={layout:"default",data:[{}],fetch:[],error:{code:"rest_no_route",message:"No route was found matching the URL and request method.",data:{status:404},statusCode:500},serverRendered:!1,routePath:"/books",config:{content:{dbHash:"b25b294c"}}}
